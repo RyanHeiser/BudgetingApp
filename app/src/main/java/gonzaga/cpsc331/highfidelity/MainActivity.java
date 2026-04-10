12923de3909a44d68926d7846e10543ae2a462f5
RyanHeiser <rmheiser808@gmail.com>
2026-04-09 19:28:03 -0700

    Login screen

---
+++ app/src/main/java/gonzaga/cpsc331/highfidelity/MainActivity.java
@@ -0,0 +1,19 @@
+package gonzaga.cpsc331.highfidelity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends ComponentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.main);
+    }
+
+}
