471c8ed345b9f5b6e4684b903474ca31acf006b0
AidAmal <Aidanjamal@gmail.com>
2026-04-20 23:09:03 -0700

    goal setters and other methods

--- app/src/main/java/gonzaga/cpsc331/highfidelity/model/Goal.java
+++ app/src/main/java/gonzaga/cpsc331/highfidelity/model/Goal.java
@@ -3,16 +3,38 @@ package gonzaga.cpsc331.highfidelity.model;
 public class Goal {
     private double goal;
 
-    private double amount;
+    private double progress;
 
     private String name;
 
+    private boolean isAchieved;
     public Goal(double goal, String name){
         this.goal = goal;
         this.name = name;
-        this.amount = 0.0;
+        this.progress = 0.0;
+        this.isAchieved = false;
     }
 
+    public String getName(){
+        return this.name;
+    }
+    public double getGoal(){
+        return this.goal;
+    }
+    public double getProgress(){
+        return this.progress;
+    }
+    public void incrementProgressBar(double increment){
+        this.progress += increment;
+    }
+    public boolean isAchieved(){
+        if(progress >= goal){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
 
 }
