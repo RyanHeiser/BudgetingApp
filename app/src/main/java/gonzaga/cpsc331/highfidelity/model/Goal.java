90c95105d2a09316f2eedce2166b3a8ff8ae98ed
AidAmal <Aidanjamal@gmail.com>
2026-04-20 22:13:57 -0700

    goals class

---
+++ app/src/main/java/gonzaga/cpsc331/highfidelity/model/Goal.java
@@ -0,0 +1,16 @@
+package gonzaga.cpsc331.highfidelity.model;
+
+public class Goal {
+    private double goal;
+
+    private double amount;
+
+    private String description;
+
+    private BudgetRow budgetRow;
+
+    public Goal(){
+
+    }
+
+}
